69e2a9e65508c61ce4cc79f672a16f68f06cb730
Vineet Kumar Singh <vineetks@ip-192-168-31-84.ap-south-1.compute.internal>
2025-12-13 00:01:31 +0530

    first commit

--- README.md
+++ README.md
@@ -58,6 +58,3 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
 # first-contact-chcc
-# first-contact-chcc
-# first-contact-chcc
-# first-contact-chcc
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+        projectId: "first-contact-chcc",
+        appId: "1:316554060244:web:0218a72e8cbc941ce2b641",
+        storageBucket: "first-contact-chcc.firebasestorage.app",
+        apiKey: "AIzaSyB5C05rCIJQ0LKMVkSRYEqW2TUSj-LLSxU",
+        authDomain: "first-contact-chcc.firebaseapp.com",
+        messagingSenderId: "316554060244",
+        measurementId: "G-H3VY723M4N",
+        // projectNumber: "316554060244",
+        // version: "2"
+      })),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
+  ]
 };
